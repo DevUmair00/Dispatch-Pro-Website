dab432694e24a74a30b371e2153b1bb630c22a7d
Dev Umair <devumair00@gmail.com>
2026-05-11 20:21:28 +0500

    Contact and Register Form Work using Fetch API and stored in DB

--- Frontend/JS/indexContact.js
+++ Frontend/JS/indexContact.js
@@ -1,31 +1,23 @@
-const form = document.getElementById("contactForm");
+const form = document.getElementById("instantContactForm");
 
 
 form.addEventListener("submit", async function (e) {
 
     e.preventDefault();
 
-    let firstName = document.getElementById("first-name").value;
 
-    let lastName = document.getElementById("last-name").value;
-
-    // Collect Form Data -> Create JS Object To Send To Server
+    // Collect Form Data
     const contactData = {
 
 
-        name: `${firstName} ${lastName}`,
+        name: document.getElementById("name").value,
         
         phone: document.getElementById("phone").value,
 
-        email: document.getElementById("email").value,
-
         truck_type: document.getElementById("truck-type").value,
-
-        no_of_trucks: document.getElementById("number-of-trucks").value,
 
         operating_state: document.getElementById("operating-lanes").value,
 
-        note: document.getElementById("additional-notes").value
     };
 
     console.log(contactData);
@@ -51,7 +43,7 @@ form.addEventListener("submit", async function (e) {
 
             console.log(data);
 
-            alert("Request Submitted Successfully");
+            alert("Quote Request Submitted Successfully");
 
             form.reset();
         }
--- Frontend/JS/register.js
+++ Frontend/JS/register.js
@@ -1,10 +1,74 @@
-const URL = "https://cat-facts.herokuapp.com/facts";
+form = document.getElementById("registerForm");
 
-const getData = async () => {
+form.addEventListener("submit", async function (e) {
 
-    console.log("Getting Data....");
-    let response = await fetch(URL);
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
-};
+    e.preventDefault();
+
+    //Collect Form Data -> Create JS Object To Send To Server
+
+    let first_name = document.getElementById("first_Name").value;
+
+    let last_name = document.getElementById("last_Name").value;
+
+
+    const registrationData = {
+
+        name: `${first_name} ${last_name}`,
+
+        phone: document.getElementById("phone").value,
+
+        email: document.getElementById("email").value,
+
+        operating_state: document.getElementById("operating-state").value,
+
+        experience: document.getElementById("years-of-experience").value,
+
+        truck_type: document.getElementById("truck-type").value,
+
+        no_of_trucks: document.getElementById("number-of-trucks").value,
+
+        mc_no: document.getElementById("mc-dot-number").value,
+
+        truck_year: document.getElementById("truck-year").value,
+
+        preferred_lane: document.getElementById("operating-Lanes").value
+    };
+
+    console.log(registrationData);
+
+    try {
+
+        const response = await fetch(
+            "http://localhost:8080/api/register",
+            {
+
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify(registrationData)
+            }
+        )
+
+        if (response.ok) {
+            const data = await response.json();
+
+            console.log(data);
+
+            alert("Quote Request Submitted Successfully");
+
+            form.reset();
+        }
+        else {
+            alert("Failed To Submit Form");
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+
+        alert("Server Error");
+    }
+});
